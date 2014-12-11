7394e298557cc32b2bbb9aac835f7720ab717c4e
Lee Danilek <lee.danilek@yale.edu>
2014-12-11 01:30:14 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,6 +7,8 @@
 - (void)initialize
 {
     // your code here
+    character = (Character *)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
 }
 
 -(void)update:(CCTime)delta
@@ -14,6 +16,9 @@
     // put update code here
 }
 
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    [character flap];
+}
 // put new methods here
 
 @end
